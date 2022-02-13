71a5fc6e0dc492a9b1ba52467e7eb94c6cf1dad4
Arshath <itizarsa@gmail.com>
2022-02-13 17:23:40 +0530

    wip

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+export const SITE = {
+	name: "Arshath",
+	title: "Arshath",
+	description: "Arshath's personal website",
+	image: "https://arshath.com/assets/img/arshath-logo.png",
+	url: "https://arshath.vercel.app",
+	github: "https://github.com/itizarsa",
+	linkedin: "https://linkedin.com/in/itizarsa",
+	email: "itizarsa@gmail.com"
+}
+
+export const PAGE_SIZE = 8
---
+++ tailwind.config.cjs
@@ -0,0 +1,3 @@
+module.exports = {
+	content: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"]
+}
